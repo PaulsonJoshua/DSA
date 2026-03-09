5b5d2504bdd408bf9e3cb8b806875f12348e8c42
Paulson Joshua <paulsonjoshua660@gmail.com>
2026-03-09 21:59:08 +0530

    Time: 8 ms (24.03%) | Memory: 47.1 MB (34.24%) - LeetSync

---
+++ 2825-minimize-string-length/minimize-string-length.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int minimizedStringLength(String s) {
+        String temp=s;
+        HashSet<Character> hs=new HashSet<>();
+        for(char ch:s.toCharArray())
+        {
+            hs.add(ch);
+        }
+        return hs.size();
+    }
+}
