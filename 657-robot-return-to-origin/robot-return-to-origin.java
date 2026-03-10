987fed07eb565dd6aef23c77f3cfae60924ddf04
Paulson Joshua <paulsonjoshua660@gmail.com>
2026-03-10 22:46:39 +0530

    Time: 8 ms (23.23%) | Memory: 45.7 MB (60.35%) - LeetSync

---
+++ 657-robot-return-to-origin/robot-return-to-origin.java
@@ -0,0 +1,27 @@
+class Solution {
+    public boolean judgeCircle(String moves) {
+        int count1=0,count2=0;
+        for(int i=0;i<moves.length();i++)
+        {
+            if(moves.charAt(i)=='U')
+            {
+                count1++;
+                
+            }
+            else if(moves.charAt(i)=='D')
+            {
+                count1--;
+                
+            }
+            else if(moves.charAt(i)=='L')
+            count2++;
+            else if(moves.charAt(i)=='R')
+            count2--;
+        }
+        if(count1==0 && count2==0)
+        {
+            return true;
+        }
+        return false;
+    }
+}
