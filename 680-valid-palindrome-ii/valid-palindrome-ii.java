88e968e8de6a76f058168edc30c7479c50a5c0b2
Paulson Joshua <paulsonjoshua660@gmail.com>
2026-05-08 23:53:19 +0530

    Time: 4 ms (99.64%) | Memory: 47.7 MB (66.85%) - LeetSync

---
+++ 680-valid-palindrome-ii/valid-palindrome-ii.java
@@ -0,0 +1,28 @@
+class Solution {
+    public boolean validPalindrome(String s) {
+        int left=0,right=s.length()-1;
+        while(left<right)
+        {
+            if(s.charAt(left)!=s.charAt(right))
+            {
+                return isPalindrome(s,left+1,right)||isPalindrome(s,left,right-1);
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+    public boolean isPalindrome(String s,int joshua,int mercy)
+    {
+        while(joshua<mercy)
+        {
+        if(s.charAt(joshua)!=s.charAt(mercy))
+        {
+            return false;
+        }
+        joshua++;
+        mercy--;
+        } 
+        return true;
+    }
+}
