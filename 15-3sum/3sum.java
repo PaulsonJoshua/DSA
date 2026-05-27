f32a0078c59ab5efcb1ff8ad5fbdd59063d36fda
Paulson Joshua <paulsonjoshua660@gmail.com>
2026-05-27 12:09:44 +0530

    Time: 29 ms (92.96%) | Memory: 59 MB (84.50%) - LeetSync

--- 15-3sum/3sum.java
+++ 15-3sum/3sum.java
@@ -3,29 +3,39 @@ class Solution {
         Arrays.sort(nums);
         List<List<Integer>> li=new ArrayList<>();
         int n=nums.length;
-        for(int i=0;i<n-2;i++)
+        for(int i=0;i<n;i++)
         {
-            int j=i+1,k=n-1;
             if(i>0 && nums[i]==nums[i-1])
             continue;
-            while(j<k)
+            else
             {
-                int sum=nums[i]+nums[j]+nums[k];
-                if(sum==0)
+                int j=i+1,k=n-1;
+                while(j<k)
                 {
-                li.add(Arrays.asList(nums[i],nums[j],nums[k]));
-                
-                j++;
-                k--;
-                
-                
-                while(j<k && nums[j]==nums[j-1])
-                {
-                    j++;
+                    int n1=nums[i]+nums[j]+nums[k];
+                    if(n1<0)
+                    {
+                        j++;
+                    }
+                    else if(n1>0)
+                    {
+                        k--;
+                    }
+                    else
+                    {
+                        li.add(Arrays.asList(nums[i],nums[j],nums[k]));
+                        j++;
+                        k--;
+                        while(j<k &&(nums[j]==nums[j-1]))
+                        {
+                            j++;
+                        }
+                        while(j<k && (nums[k]==nums[k+1]))
+                        {
+                            k--;
+                        }
+                    }
                 }
-                }
-                else if (sum<0) j++;
-                 else k--;
             }
         }
           return li;   
